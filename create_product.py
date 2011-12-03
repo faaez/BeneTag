a1ef5940c20d24356dec32cacc7de86c41a762fc
Jeffrey Hodes <jeffreyhodes@gmail.com>
2011-12-03 18:05:01 -0500

    product creation page

---
+++ create_product.py
@@ -0,0 +1,29 @@
+import os
+
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
+import entities
+
+"""
+Creates a form for Producers to enter information 
+about a Product
+"""
+class CreateProductPage(webapp.RequestHandler):
+    def get(self):
+        template_values = {}
+        path = os.path.join(os.path.dirname(__file__), 'createProduct.html')
+        self.response.out.write(template.render(path, template_values))
+
+"""
+Page that stores Product in datastore
+"""
+class StoreProductPage(webapp.RequestHandler):
+    def post(self):
+        _id = self.request.get('id')
+        _name = self.request.get('name')
+        key = db.Key.from_path('Product', 'product')
+        p = entities.Product(parent=key, id=_id, name=_name)
+        p.put()
+        self.response.out.write('Created a product!')
--- hellobenetag.py
+++ hellobenetag.py
@@ -1,3 +1,15 @@
-print 'Content-Type: text/plain'
-print ''
-print 'Welcome to Benetag!'
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+import create_product
+
+application = webapp.WSGIApplication([
+  ('/product', create_product.CreateProductPage),
+  ('/storeProduct', create_product.StoreProductPage)
+], debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == '__main__':
+  main()
