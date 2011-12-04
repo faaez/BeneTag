cb6e8a007e0e421ba1a9e24a691f776ffc5ec838
Farhan Abrol <fabrol92@gmail.com>
2011-12-04 00:50:03 -0500

    viewfactory

--- hellobenetag.py
+++ hellobenetag.py
@@ -1,13 +1,14 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-import create_product, view_product, create_qr
+import create_product, view_product, create_qr, view_factory
 
 application = webapp.WSGIApplication([
   ('/createproduct', create_product.CreateProductPage),
   ('/storeproduct', create_product.StoreProductPage),
   ('/view', view_product.ViewProduct),
-  ('/qr', create_qr.CreateQrPage)
+  ('/qr', create_qr.CreateQrPage),
+  ('/viewfactory', view_factory.ViewFactory),
 ], debug=True)
 
 def main():
