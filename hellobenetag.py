4020304a7b6a813d393c46e8d1c491b30e0a20d0
Faaez <faaez.ulhaq@gmail.com>
2011-12-03 19:42:22 -0500

    changing camel case

--- hellobenetag.py
+++ hellobenetag.py
@@ -1,11 +1,17 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-import create_product
+import create_product, view_product
 
 application = webapp.WSGIApplication([
+<<<<<<< HEAD
+  ('/product', create_product.CreateProductPage),
+  ('/storeProduct', create_product.StoreProductPage),
+  ('/view', view_product.ViewProduct),
+=======
   ('/createproduct', create_product.CreateProductPage),
   ('/storeproduct', create_product.StoreProductPage)
+>>>>>>> master
 ], debug=True)
 
 def main():
