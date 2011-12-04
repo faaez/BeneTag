429c23f73a7e48ba5e6cb5317d46a2fa38ed68c0
Farhan Abrol <fabrol92@gmail.com>
2011-12-04 02:06:21 -0500

    enjoy this shit

--- entities.py
+++ entities.py
@@ -1,22 +1,5 @@
 from google.appengine.ext import db
 
-"""
-Data type representing a product with a BeneTag
-"""
-class Product(db.Model):
-    name = db.StringProperty(required=True)
-    picture = db.BlobProperty()
-    producerName = db.StringProperty()
-    factoryMade = db.ReferenceProperty()
-    badges = db.ListProperty(db.Key)
-
-"""
-Data type representing a producer
-"""
-class Producer(db.Model):
-	factories = db.ListProperty(db.Key)
-	name = db.StringProperty(required=True)
-	profileOwner = db.UserProperty(required=True)
 
 """
 Data type representing a factory
@@ -29,11 +12,29 @@ class Factory(db.Model):
 	location = db.GeoPtProperty()
 
 """
+Data type representing a product with a BeneTag
+"""
+class Product(db.Model):
+    name = db.StringProperty(required=True)
+    picture = db.BlobProperty()
+    producerName = db.StringProperty()
+    factoryMade = db.ReferenceProperty(Factory)
+    badges = db.ListProperty(db.Key)
+
+"""
+Data type representing a producer
+"""
+class Producer(db.Model):
+	factories = db.ListProperty(db.Key)
+	name = db.StringProperty(required=True)
+	profileOwner = db.UserProperty(required=True)
+
+"""
 Data type representing a worker
 """
 class Worker(db.Model):
 	name = db.StringProperty()
-	factory = db.ReferenceProperty()
+	factory = db.ReferenceProperty(Factory)
 	profile = db.TextProperty()
 	picture = db.BlobProperty()
 
