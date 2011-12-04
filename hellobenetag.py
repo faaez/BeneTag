ede8f63c0087525716934beaf65f715046c1d14e
Faaez <faaez.ulhaq@gmail.com>
2011-12-03 19:47:34 -0500

    changing camel case

--- hellobenetag.py
+++ hellobenetag.py
@@ -5,16 +5,9 @@ import create_product, view_product
 
 application = webapp.WSGIApplication([
 <<<<<<< HEAD
-  ('/product', create_product.CreateProductPage),
-  ('/storeProduct', create_product.StoreProductPage),
-  ('/view', view_product.ViewProduct),
-<<<<<<< HEAD
-=======
   ('/createproduct', create_product.CreateProductPage),
-  ('/storeproduct', create_product.StoreProductPage)
->>>>>>> master
-=======
->>>>>>> 1f7ce105e99bf0a51ae65207adfaaccf9104a5fa
+  ('/storeproduct', create_product.StoreProductPage),
+  ('/view', view_product.ViewProduct),
 ], debug=True)
 
 def main():
