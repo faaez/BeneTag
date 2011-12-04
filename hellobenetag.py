4d50a0ca8cbe8093a6ce91cb3e985a2da2cad1d3
Jeffrey Hodes <jeffreyhodes@gmail.com>
2011-12-03 21:26:41 -0500

    asdfa

--- hellobenetag.py
+++ hellobenetag.py
@@ -1,12 +1,13 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-import create_product, view_product
+import create_product, view_product, create_qr
 
 application = webapp.WSGIApplication([
   ('/createproduct', create_product.CreateProductPage),
   ('/storeproduct', create_product.StoreProductPage),
   ('/view', view_product.ViewProduct),
+  ('/qr', create_qr.CreateQrPage)
 ], debug=True)
 
 def main():
