517191efc5b8a8afc8e3bf550cb4593b2c616094
Jeffrey Hodes <jeffreyhodes@gmail.com>
2011-12-04 00:57:09 -0500

    home page!

--- hellobenetag.py
+++ hellobenetag.py
@@ -1,12 +1,14 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+import home
 import create_product
 import view_product 
 import create_qr
 import create_factory
 
 application = webapp.WSGIApplication([
+  ('/', home.HomePage),
   ('/createproduct', create_product.CreateProductPage),
   ('/storeproduct', create_product.StoreProductPage),
   ('/createfactory', create_factory.CreateFactoryPage),
